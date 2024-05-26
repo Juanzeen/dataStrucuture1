006849362003a5923f74e16a3df7fdb54d65bdd7
juanzeen <jziignerdev@gmail.com>
2024-05-26 17:12:06 -0300

    putting the phrase

--- LE2/2.c
+++ LE2/2.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#define MAX  3
+#define LENGTH  32
 
-void copyStackReverse(char stack1[MAX], char stack2[MAX]){
-    int i = 0, j = MAX - 1;
-    for(i ; i < MAX; i++){
+void copyStackReverse(char stack1[LENGTH], char stack2[LENGTH]){
+    int i = 0, j = LENGTH - 1;
+    for(i ; i < LENGTH; i++){
     stack2[i] = stack1[j];
     j--;
     }
 }
 
-void show(char stack[MAX]){
-    for(int i = 0; i < MAX; i++){
-        if(i < MAX) printf("%c |", stack[i]);
+void show(char stack[LENGTH]){
+    for(int i = 0; i < LENGTH; i++){
+        if(i < LENGTH) printf("%c |", stack[i]);
         else{
             printf("%c", stack[i]);
         }
     }
 }
 
-bool checkPalindrome(char stack1[MAX], char stack2[MAX]){
-    int j = MAX - 1;
+bool checkPalindrome(char stack1[LENGTH], char stack2[LENGTH]){
+    int j = LENGTH - 1;
     bool itsPalindrome = true;
     for(j; j>0; j--){
         if(stack1[j] == stack2[j]){
@@ -35,9 +35,8 @@ bool checkPalindrome(char stack1[MAX], char stack2[MAX]){
 }
 
 int main(void){
-    char myString[MAX];
-    char auxArray[MAX];
-    scanf("%s", &myString);
+    char myString[LENGTH] = "socorrammesubinoonibusemmarrocos"; //Socorram-me, subi no ônibus em Marrocos
+    char auxArray[LENGTH];
     copyStackReverse(myString, auxArray);
     printf("%s", checkPalindrome(myString, auxArray)? "true\n": "false\n");
 
